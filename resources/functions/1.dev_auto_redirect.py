af02eb2e2df3593ed1063717a0e8c29f89f69d4a
RecentRichRail <Matthewspicer.18@gmail.com>
2024-08-16 19:32:37 +0000

    Bug fixes

--- resources/functions/1.dev_auto_redirect.py
+++ resources/functions/1.dev_auto_redirect.py
@@ -2,17 +2,17 @@ from flask import current_app
 import requests
 
 def run(data):
-    if data["user_active_env"] != "dev":
+    if data["user_active_env"] != "prod":
         for permission in data['user_permissions']:
             if permission['permission_name'] == "dev" and permission['permission_level'] <= 999:
                 response = requests.get(f"{current_app.dev_server}/external/status")
-                dev_server_status = response.json()
+                # dev_server_status = response.json()
 
-                if dev_server_status == None:
-                    return {"function_triggered": False}
-                
-                elif dev_server_status['status'] == 200:
+                if response.status_code == 200:
                     dev_user_query = data['user_query']['original_request']
                     return {"funtion_triggered": True, "funtion_return": f"https://dev.spicerhome.net/internal/search?q={dev_user_query}"}
+                
+                else:
+                    return {"function_triggered": False}
             
     return {"function_triggered": False}
